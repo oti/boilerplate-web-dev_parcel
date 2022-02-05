a472a1fcdd17194cec3534a602a300f6a794558a
oti <otiext@gmail.com>
2022-02-05 23:54:00 +0900

    feat(script): ViewportFit.js を追加した

--- src/script/main.ts
+++ src/script/main.ts
@@ -3,8 +3,12 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import { InView } from "./module/InView";
+import { ViewportFit } from "./module/ViewportFit";
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const inView = new InView();
   inView.init();
+
+  const viewportFit = new ViewportFit();
+  viewportFit.init();
 });
